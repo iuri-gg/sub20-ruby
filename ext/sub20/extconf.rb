d9ba437ab703af1e1b67a06ddfc76d48d1f3b3c9
Iuri Gagnidze <ocha@ocha-vm.(none)>
2012-01-17 21:42:50 -0600

    -- WIP

--- ext/sub20/extconf.rb
+++ ext/sub20/extconf.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 require 'mkmf'
-$CFLAGS << ' -DLIBUSB_1_0 -I/usr/local/include/libusb-1.0'
+$CFLAGS << ' -DLIBUSB_1_0 -I/usr/local/include/libusb-1.0 -O2 -Wall -Werror -g -I../src -I../boot/vnd/fw -I.'
+$LDFLAGS << ' -L/usr/local/lib -lusb-1.0 -lm '
 
 create_makefile('sub20')
--- ext/sub20/sub20.c
+++ ext/sub20/sub20.c
@@ -2,32 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <libusb.h>
 
 #include "ruby.h"
 #include "libsub.h"
 
-static VALUE s_init(VALUE self)
-{
-  VALUE arr;
-
-  arr = rb_ary_new();
-  rb_iv_set(self, "@arr", arr);
-  return self;
-}
-
-static VALUE s_add(VALUE self, VALUE anObject)
-{
-  VALUE arr;
-
-  arr = rb_iv_get(self, "@arr");
-  rb_ary_push(arr, anObject);
-  return arr;
-}
 
 VALUE cSub20;
 
+static VALUE s_init(VALUE self) {
+    VALUE s_dev;
+    sub_handle* fd = NULL ;
+    struct usb_device* dev = NULL;
+
+    /* Open USB device */
+    fd = sub_open( dev );
+
+    s_dev = Data_Wrap_Struct(cSub20, 0, free, fd);
+    rb_iv_set(self, "@device", s_dev);
+    return self;
+}
+
+static VALUE s_lcd_write(VALUE self, VALUE str) {
+    sub_handle* fd = NULL;
+    VALUE s_dev;
+    char strr = 'a';
+    s_dev = rb_iv_get(self, "@device");
+    Data_Get_Struct(s_dev,sub_handle,fd);
+    sub_lcd_write(fd, &strr);
+    return str;
+}
 void Init_sub20() {
-  cSub20 = rb_define_class("Sub20", rb_cObject);
-  rb_define_method(cSub20, "initialize", s_init, 0);
-  rb_define_method(cSub20, "add", s_add, 1);
+    cSub20 = rb_define_class("Sub20", rb_cObject);
+    rb_define_method(cSub20, "initialize", s_init, 0);
+    rb_define_method(cSub20, "lcd_write", s_lcd_write, 1);
 }
