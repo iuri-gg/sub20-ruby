b2f9839b85e5bdff4f1d6a9c412847be00c15fc2
Iuri Gagnidze <igagnidz@gmail.com>
2012-03-19 22:47:51 -0500

    - gpio_config working now

--- ext/sub20/sub20.c
+++ ext/sub20/sub20.c
@@ -60,6 +60,17 @@ static VALUE s_strerror(VALUE self, VALUE error_code) {
     return rb_str_new2(sub_strerror(FIX2INT(error_code)));
 }
 
+static VALUE s_gpio_config(VALUE self, VALUE set, VALUE mask) {
+    sub_handle* fd = NULL;
+    VALUE s_dev;
+    sub_int32_t read_config;
+    int ret_code;
+    s_dev = rb_iv_get(self, "@device");
+    Data_Get_Struct(s_dev,sub_handle,fd);
+    ret_code = sub_gpio_config(fd, FIX2INT(set), &read_config, FIX2INT(mask));
+    return INT2FIX(ret_code);
+}
+
 void Init_sub20() {
     cSub20 = rb_define_class("Sub20", rb_cObject);
     rb_define_method(cSub20, "initialize", s_init, 0);
@@ -67,4 +78,5 @@ void Init_sub20() {
     rb_define_method(cSub20, "get_serial_number", s_get_serial_number, 0);
     rb_define_method(cSub20, "get_product_id", s_get_product_id, 0);
     rb_define_method(cSub20, "strerror", s_strerror, 1);
+    rb_define_method(cSub20, "gpio_config", s_gpio_config, 2);
 }
